e27ed1e31388a7bbc1fde482bb49df4737ac7ce2
MariuszMarczewski <mariusz.marczewski1@poczta.fm>
2020-11-20 15:56:28 +0100

    add element

--- src/main/java/pl/marczewski/asweek4thymeleaf/CarController.java
+++ src/main/java/pl/marczewski/asweek4thymeleaf/CarController.java
@@ -3,6 +3,8 @@ package pl.marczewski.asweek4thymeleaf;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,16 +13,31 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    @GetMapping("/car")
-    public String getCar(Model model){
+    private List<Car> cars;
+
+    public CarController() {
         Car car1 = new Car("BMW", "i8");
         Car car2 = new Car("Volvo", "S90");
         Car car3 = new Car("Mercedes", "E");
         Car car4 = new Car("Audi", "A6");
 
-        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+        cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+    }
 
+    @GetMapping("/car")
+    public String getCar(Model model){
         model.addAttribute("cars", cars);
+        model.addAttribute("newCar", new Car());
         return "car";
     }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car){
+        cars.add(car);
+        return "redirect:/car";
+    }
+
 }
